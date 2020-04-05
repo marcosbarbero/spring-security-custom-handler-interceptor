ba9ed157cd5e051961b4d2f8372f464074b50df4
Marcos Barbero <marcos.hgb@gmail.com>
2020-04-05 12:58:18 +0200

    Initial commit

---
+++ src/main/java/com/marcosbarbero/sec/SpringSecurityCustomHandlerInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.marcosbarbero.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCustomHandlerInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCustomHandlerInterceptorApplication.class, args);
+	}
+
+}
